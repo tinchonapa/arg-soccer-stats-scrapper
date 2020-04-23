b617346e3d12d6207781bb6faefe0e27ecf2315a
Martin Naparstek <tinchonapa@gmail.com>
2020-04-22 18:14:14 -0600

    Finalized statsScrapper to effectively retrieve game stats for gk and players

--- statsScrapper.js
+++ statsScrapper.js
@@ -29,9 +29,10 @@ function goThroughTeams(){
 function getPlayerStats() {
   pause(1, () => {
     // create script to grab data and ignore unnecesary fields(titles & players without stats)
+    var gkStats, plyrStats;
     var extractGK = function (team) {
-      var length = document.getElementsByClassName('sortable')[0].childNodes[0].childNodes.length;
-      var arrOfElements = document.getElementsByClassName('sortable')[0].childNodes[0].childNodes;
+      var length = document.getElementsByClassName('sortable')[0].childNodes[0].childNodes.length; // finds amount of rows
+      var arrOfElements = document.getElementsByClassName('sortable')[0].childNodes[0].childNodes; // variable to access rows easier
       var storage = [];
       var begIndex = 0;
       var id, gp, min, gc, gaa, pg, pa, pksa, saves, shots, sog, ssa, cs, yc, rc = ''; // variables for values of obj
@@ -59,35 +60,49 @@ function getPlayerStats() {
       return storage;
     }
 
-    /*var extractPlayers = function (team) {
-      var length = document.getElementsByClassName('sortable')[1].childNodes[0].childNodes.length;
-      var arrOfElements = document.getElementsByClassName('sortable')[1].childNodes[0].childNodes;
-      var storage = [];
-      var begIndex = 0;
-      var id, name, numb, pos, height, weight, dob, pob = ''; // variables for values of obj
-      for(var i = 0; i < length; i++) {
-          if ( (arrOfElements[i].cells.length > 1) && (arrOfElements[i].cells[0].innerText.length !== 0) ) { // players without shirt# mean that they didn't play. Skip them
-              if ( arrOfElements[i].cells[0].innerText !== "No." ) { // skip header rows
-                  numb = arrOfElements[i].cells[0].innerText;
-                  begIndex = arrOfElements[i].cells[1].firstChild.href.indexOf('?') + 8;
-                  id = arrOfElements[i].cells[1].firstChild.href.slice(begIndex);
-                  name = arrOfElements[i].cells[1].innerText;
-                  pos = arrOfElements[i].cells[2].innerText;
-                  height = arrOfElements[i].cells[3].innerText;
-                  weight = arrOfElements[i].cells[4].innerText;
-                  dob = arrOfElements[i].cells[5].innerText;
-                  pob = arrOfElements[i].cells[6].innerText;
-                  storage.push({"id": id, "numb": numb, "name": name, "pos": pos, "height": height, "weight": weight, "dob": dob, "pob": pob, "team": team});
-                  // console.log('Storage: ', storage)
-              }
-        }
-      }
-      return storage;
-    }*/
+    // run extractGK script and store return array elements into final array
     driver.executeScript(extractGK, team).then((result) => {
       playersData.push(...result);
     });
-    count++;
+    
+    var extractPlayers = function (team) {
+      var length = document.getElementsByClassName('sortable')[1].childNodes[0].childNodes.length; // finds amount of rows
+      var arrOfElements = document.getElementsByClassName('sortable')[1].childNodes[0].childNodes; // variable to access rows
+      var storagePlyrs = [];
+      var begIndex = 0;
+      var id, gp, min, g, a, pks, shots, sot, sota, yc, rc, fc, fs, crosses, of = '';
+      for ( var i = 1; i < length - 1; i++) {
+        if (arrOfElements[i].cells[1].innerText !== 'P' ) {
+          begIndex = arrOfElements[i].cells[0].firstChild.href.indexOf('?') + 8;
+          id = arrOfElements[i].cells[0].firstChild.href.slice(begIndex);
+          gp = arrOfElements[i].cells[2].innerText;
+          min = arrOfElements[i].cells[3].innerText;
+          g = arrOfElements[i].cells[4].innerText;
+          a = arrOfElements[i].cells[5].innerText;
+          pks = arrOfElements[i].cells[6].innerText;
+          shots = arrOfElements[i].cells[7].innerText;
+          sot = arrOfElements[i].cells[8].innerText;
+          sota = arrOfElements[i].cells[9].innerText;
+          yc = arrOfElements[i].cells[10].innerText;
+          rc = arrOfElements[i].cells[11].innerText;
+          fc = arrOfElements[i].cells[12].innerText;
+          fs = arrOfElements[i].cells[13].innerText;
+          crosses = arrOfElements[i].cells[14].innerText;
+          of = arrOfElements[i].cells[15].innerText;
+          storagePlyrs.push({"id": id, "gp":gp, "min":min, "g":g,"a":a,"pks":pks,"shots":shots,
+            "sot":sot,"sota":sota,"yc":yc,"rc":rc,"fc":fc,"fs":fs,"crosses":crosses,"of": of, "team":team});
+          // console.log('storagePlyrs: ', storagePlyrs);
+        }
+      }
+      return storagePlyrs;
+    }
+    
+    // run extractPlayers script and store return array elements into final array
+    driver.executeScript(extractPlayers, team).then((result) => {
+      playersData.push(...result);
+    });
+
+    count++; // counter for iteration through teams
     console.log(count)
     pause(1, goThroughTeams)
   })
